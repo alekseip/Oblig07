bb1c26bcdf22d1b7081bc63b5e3fff498bb9cd43
alekseip <aleksei.pedersen@gmail.com>
2015-03-24 19:17:34 +0100

    Obblig 7

--- Oblig7.java
+++ Oblig7.java
@@ -1,31 +1,31 @@
+// Skrevet av Aleksei, Carl
 import java.util.Scanner;
 import java.io.*;
 
 public class Oblig7{
-    public static void main(String args[]){
+    public static void main(String args[]) throws FinnException {
 
 
         //ordrestyrt
         Scanner in = new Scanner(System.in);
-        String info ="Oblig 7, INF1010\n\t--- Leger og Resepter ---\n\t1: Les in data fra fil\n\t2: Skriv data til fil\n\t3: Print all data til skjerm\n\t4: Opprett et nytt legemiddel\n\t5: Opprett en ny lege\n\t6: Oprett en ny person\n\t7: Opprett en ny resept\n\t8: Hent legemiddelet for en resept\n\t9: Velg en statistik til aa skirve ut\n\t0: Exit\n\tVelg 0-9: ";
+        String info ="Oblig 7, INF1010\n\t--- Leger og Resepter ---\n\t1: Skriv data til fil\n\t2: Print all data til skjerm\n\t3: Opprett et nytt legemiddel\n\t4: Opprett en ny lege\n\t5: Oprett en ny person\n\t6: Opprett en ny resept\n\t7: Hent legemiddelet for en resept\n\t8: Velg en statistik til aa skirve ut\n\t0: Exit\n\tVelg 0-9: ";
 
 
         int a = 10;
         Ordre o = new Ordre();
-
+        o.lesFil();
         while(a != 0){
             System.out.print(info);
             a = in.nextInt();
             switch(a){
-                case 1: o.lesFil(); break;
-                case 2: o.printDataTilFil();break;
-                case 3: o.printData(); break;
-                case 4: o.nyttLegemiddel(); break;
-                case 5: o.nyLege(); break;
-                case 6: o.nyPerson(); break;
-                case 7: o.nyResept(); break;
-                case 8: System.out.println("hente legemiddelet for en resept"); break;
-                case 9: System.out.println("valg av statistiker"); break;
+                case 1: o.printDataTilFil();break;
+                case 2: o.printData(); break;
+                case 3: o.nyttLegemiddel(); break;
+                case 4: o.nyLege(); break;
+                case 5: o.nyPerson(); break;
+                case 6: o.nyResept(); break;
+                case 7: o.hentLegemiddel(); break;
+                case 8: o.statestikk(); break;
             }
         }
     }
@@ -43,9 +43,8 @@ class Ordre{
 
             while(fileIn.hasNext()){
                 String s = fileIn.nextLine();
-                if(s.startsWith("﻿# Personer")){
+                if(s.startsWith("# Personer")){
                     s = fileIn.nextLine();
-
                     while(!s.startsWith("#")&& !s.equalsIgnoreCase("")){
 
                         String[] ord = s.split(", ");
@@ -57,7 +56,6 @@ class Ordre{
                         personerTabell.settIn(new Personer(unik,navn,fnr,adr,post), unik);
 
                         s = fileIn.nextLine();
-
                     }
                 }
 
@@ -116,7 +114,7 @@ class Ordre{
                                     legemiddlerTabell.returner(legemnr).pris(),reit, unik));
 
                         }
-                        if (farge.equalsIgnoreCase("blå"))
+                        if (farge.equalsIgnoreCase("blaa"))
                         { personResepter.settInn(new BlaaResept(legemiddlerTabell.returner(legemnr),legerSliste.getE(lege),personerTabell.returner(unikpersnr),
                                 legemiddlerTabell.returner(legemnr).pris(),reit, unik));
                             legeResepter.settInn(new BlaaResept(legemiddlerTabell.returner(legemnr),legerSliste.getE(lege),personerTabell.returner(unikpersnr),
@@ -137,19 +135,19 @@ class Ordre{
         if (l instanceof TypeAMixtur){return "mikstur, a";}
         if (l instanceof TypeBMixtur){return "mikstur, b";}
         if (l instanceof TypeCMixtur){return "mikstur, c";}
-        if (l instanceof TypeAPiller){return "piller, a";}
-        if (l instanceof TypeBPiller){return "piller, b";}
-        if (l instanceof TypeCPiller){return "piller, c";}
+        if (l instanceof TypeAPiller){return "pille, a";}
+        if (l instanceof TypeBPiller){return "pille, b";}
+        if (l instanceof TypeCPiller){return "pille, c";}
         return null;
     }
 
     public String finnMengde(Legemiddler l){
-        if (l instanceof TypeAMixtur){return ((TypeAMixtur) l).getMixtur()+ "";}
+        if (l instanceof TypeAMixtur){return ((TypeAMixtur) l).getMixtur()+"";}
         if (l instanceof TypeBMixtur){return ((TypeBMixtur) l).getMixtur()+"";}
         if (l instanceof TypeCMixtur){return ((TypeCMixtur) l).getMixtur()+"";}
         if (l instanceof TypeAPiller){return ((TypeAPiller) l).getPilleribox()+"";}
         if (l instanceof TypeBPiller){return ((TypeBPiller) l).getPilleribox()+"";}
-        if (l instanceof TypeCPiller){return ((TypeCPiller) l).getPilleribox() +"";}
+        if (l instanceof TypeCPiller){return ((TypeCPiller) l).getPilleribox()+"";}
         return null;
     }
 
@@ -159,7 +157,7 @@ class Ordre{
     }
 
     public String blaaTest(Resepter r){
-        if (r instanceof BlaaResept){return "blå";}
+        if (r instanceof BlaaResept){return "blaa";}
         return "hvit";
     }
 
@@ -196,7 +194,7 @@ class Ordre{
             write.println();
             write.println("# Legemidler");
             for (Legemiddler l : legemiddlerTabell){
-                write.println(l.getUniknum()+", "+ l.getNavn()+", "+ finnType(l)+", " +l.pris()+", "+finnMengde(l)+", "+l.getVirkestoff()+", "+ l.getStyrke());
+                write.println(l.getUniknum() + ", " + l.getNavn() + ", " + finnType(l) + ", " + l.pris() + ", " + finnMengde(l) + ", " + l.getVirkestoff() + ", " + l.getStyrke());
             }
             write.println();
             write.println("# Leger");
@@ -206,7 +204,7 @@ class Ordre{
             write.println();
             write.println("# Resepter");
             for (Resepter r : legeResepter){
-                write.println(r.getReseptnummer() + ", " + blaaTest(r) + ", " + r.getPasient().getUniknummer()+", " + r.getLege().getNavnLege() + ", " + r.getLegemiddel().getUniknum() + ", " + r.getReit());
+                write.println(r.getReseptnummer() + ", " + blaaTest(r) + ", " + r.getPasient().getUniknummer() + ", " + r.getLege().getNavnLege() + ", " + r.getLegemiddel().getUniknum() + ", " + r.getReit());
             }
             write.println();
             write.println("# Stop");
@@ -279,7 +277,7 @@ class Ordre{
     }
 
     public void nyResept (){
-        System.out.println("Farge(blå,hvit)");
+        System.out.println("Farge(blaa,hvit)");
         String farge = scanner.nextLine().toLowerCase();
         System.out.println("Skriv inn uniknummer");
         int unikpn = 0;
@@ -325,7 +323,7 @@ class Ordre{
                     legemiddlerTabell.returner(legemnr).pris(),reit, unik));
 
         }
-        if (farge.equalsIgnoreCase("blå"))
+        if (farge.equalsIgnoreCase("blaa"))
         { personResepter.settInn(new BlaaResept(legemiddlerTabell.returner(legemnr),legerSliste.getE(legeNavn),personerTabell.returner(unikpersnr),
                 legemiddlerTabell.returner(legemnr).pris(),reit, unik));
             legeResepter.settInn(new BlaaResept(legemiddlerTabell.returner(legemnr),legerSliste.getE(legeNavn),personerTabell.returner(unikpersnr),
@@ -335,4 +333,140 @@ class Ordre{
 
 
     }
+    public void hentLegemiddel () throws FinnException {
+        System.out.println("Tast inn reseptnummer");
+        int nummer = scanner.nextInt();
+        scanner.nextLine();
+        int teller=0;
+        for(Resepter r : legeResepter){
+            if (r!=null){teller++;}
+        }
+        if (nummer > teller){
+            System.out.println("Resepten finnes ikke");}
+
+        if (legeResepter.finn(nummer).getReit()>0){
+            System.out.println("Here er legemidlet:"+ legeResepter.finn(nummer).getLegemiddel().getNavn());
+            legeResepter.finn(nummer).setReit(legeResepter.finn(nummer).getReit()-1);
+        }
+
+        if (legeResepter.finn(nummer).getReit()==0){
+            System.out.println("Ugyldig resept");}
+
+    }
+
+    public void statestikk(){
+        System.out.print("1.Vanedannede i Oslo\n2.Personens blaa\n3.Mikstur for legen\n4.Missbruk\n\t:");
+        int innputt = scanner.nextInt();
+        scanner.nextLine();
+
+            switch (innputt){
+                case 1: vanedannende(); break;
+                case 2: blaaPerson();break;
+                case 3: miksturLegene();break;
+                case 4: missbruk();break;
+            }
+
+
+    }
+    public void vanedannende (){
+        int antallvanedannende=0;
+        int antallOslo=0;
+        for (Resepter r : legeResepter){
+            if (r.getLegemiddel() instanceof TypeBPiller|| r.getLegemiddel() instanceof TypeBMixtur){
+                antallvanedannende++;
+                if (r.getPasient().getPostnr()<1300){
+                    antallOslo++;
+                }
+
+            }
+        }
+        System.out.println("Antall vanedanende er: "+antallvanedannende);
+        System.out.println("Antall vanedanende i Oslo: " +antallOslo);
+    }
+    public void blaaPerson (){
+        System.out.println("Skriv inn personnummer/uniknummer");
+        long nummer = scanner.nextLong();
+        scanner.nextLine();
+        int test =0;
+        for (Personer p : personerTabell){
+            if (nummer == p.getFdnummer() || nummer == p.getUniknummer()){
+                test=1;
+                for (Resepter r : personResepter){
+                    if (nummer == r.getPasient().getFdnummer()||nummer == r.getPasient().getUniknummer()){
+                        if (r instanceof BlaaResept){
+                            r.printResept("blaa");
+                        }
+                    }
+                }
+            }
+        }
+        if (test==0){System.out.println("Finner ikke person");}
+    }
+
+    public void miksturLegene (){
+        System.out.println("Skriv inn navnet til legen");
+        String navn = scanner.nextLine();
+        int mengdevirkestoff = 0;
+        int mengdemikstur = 0;
+        int mengdepiller = 0;
+        for (Resepter r : legeResepter){
+            if (r.getLege().getNavnLege().equalsIgnoreCase(navn)){
+                if (r.getLegemiddel() instanceof Mikstur){
+                       if (r instanceof BlaaResept){
+                           r.printResept("blaa");
+                       }
+                       else {r.printResept("hvit");}
+                }
+
+            mengdevirkestoff  = mengdevirkestoff+ r.getLegemiddel().getVirkestoff();
+
+                if (r.getLegemiddel() instanceof Mikstur ){
+                    mengdemikstur = mengdemikstur + r.getLegemiddel().getVirkestoff();
+                }
+                if (r.getLegemiddel() instanceof Piller){
+                    mengdepiller = mengdepiller+r.getLegemiddel().getVirkestoff();
+                }
+            }
+        }
+        System.out.println("Totalt virkestoff " + mengdevirkestoff);
+        System.out.println("Totalt virkestoff i mikstur " + mengdemikstur);
+        System.out.println("Totalt virkestoff i piller " + mengdepiller);
+    }
+    public void missbruk (){
+       for (Leger l : legerSliste )   {
+           int antallnark = 0;
+        for (Resepter r : legeResepter ){
+               if (r.getLege() == l){
+                   if (r.getLegemiddel() instanceof TypeAMixtur || r.getLegemiddel() instanceof TypeAPiller){
+                       antallnark++;
+
+                   }
+
+               }
+            }
+        if (antallnark > 0){
+            System.out.println(l.getNavnLege() +" antall narkotiske resepter "+ antallnark);
+        }
+    }
+        System.out.println("...........................................");
+        for (Personer p : personerTabell){
+        int antallnarkotika = 0;
+
+       for (Resepter r : personResepter){
+           if(r.getPasient() == p){
+               if (r.getLegemiddel() instanceof TypeAMixtur || r.getLegemiddel() instanceof TypeAPiller){
+                   antallnarkotika++;
+               }
+           }
+       }
+        if (antallnarkotika>0){
+            System.out.println(p.getNavn() + " antall narkotiske resepter " + antallnarkotika);
+        }
+    }
+
+
+    }
 }
+/* Hvis denne blir brukt mye, bør man lage en egen liste for å finne antall lettere, og få det til å
+mer oversiktlig ut ved utskriving. Burde skrive en adversel hvis en person har mere en x antall narkotiske resepter
+*/
--- Resepter.java
+++ Resepter.java
@@ -6,6 +6,7 @@ public class Resepter {
     private Personer pasient;
     private int pris;
     private int reit;
+    private String farge="hvit";
 
     public Resepter(Legemiddler legemiddel, Leger lege, Personer pasient, int pris, int reit, int reseptnummer){
         this.legemiddel=legemiddel;
@@ -22,6 +23,11 @@ public class Resepter {
     public Personer getPasient(){return pasient;}
     public int getPris(){return pris;}
     public int getReit(){return reit;}
+    public void setReit (int i){reit=i;}
+    public String getFarge(){return farge;}
+    public void printResept (String b) {
+        System.out.println(reseptnummer+ ", "+ b +", "+ pasient.getUniknummer()+", " + lege.getNavnLege()+", " + legemiddel.getUniknum()+", " + reit);
+    }
 
 }
 // Valgte aa lage BlaaResept som en subclasse for Resepter, resepter i dette tilfelle vill vaere hviteresepter, men kan
@@ -29,6 +35,7 @@ public class Resepter {
 class BlaaResept extends Resepter{
         private int pris;
         private int reseptnummer;
+        private String farge = "blaa";
     public BlaaResept(Legemiddler legemiddel, Leger lege, Personer pasient, int pris, int reit, int reseptnummer){
         super(legemiddel,lege,pasient,pris,reit,reseptnummer);
         this.pris=pris;
@@ -36,4 +43,5 @@ class BlaaResept extends Resepter{
         this.reseptnummer = reseptnummer;
     }
     public int getPris(){return pris;}// hvis det ikke fungerer sett this.pris
+    public String getFargeBlaa (){return farge; }
 }
